19deed91e7b3c7613fd8d8eb4f76d1fc6c87dec6
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-07-08 13:49:02 -0700

    Added info for first practice problem

--- Projects/actions and outlets/actions and outlets/ViewController.swift
+++ Projects/actions and outlets/actions and outlets/ViewController.swift
@@ -15,12 +15,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var appTitle2: UILabel!
     
+    @IBOutlet weak var textFieldTwo: UITextField!
     @IBAction func submitButtonPressed(_ sender: UIButton) {
         if let newTitle = textField.text {
             appTitle.text = newTitle
         }
-        
+        if let newTitleTwo = textFieldTwo.text {
+            appTitle2.text = newTitleTwo
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
