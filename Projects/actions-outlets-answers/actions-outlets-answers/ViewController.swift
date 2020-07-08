fb9fb0c059bef71148d99b6a7e338cb66ad3eac1
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-07-08 11:08:08 -0700

    Initial Commit of Actions and Outlets Answers

---
+++ Projects/actions-outlets-answers/actions-outlets-answers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  actions-outlets-answers
+//
+//  Created by Ashanti Williams on 7/8/20.
+//  Copyright © 2020 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
