2f436d244fdcf0c438b5a2a9239755953c728325
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-07-08 13:58:40 -0700

    added the stuff for the first practice problem

--- Projects/actions and outlets/actions and outlets/ViewController.swift
+++ Projects/actions and outlets/actions and outlets/ViewController.swift
@@ -19,13 +19,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textFieldTwo: UITextField!
     @IBAction func submitButtonPressed(_ sender: UIButton) {
+       //the code on lines 23-28 will change the text of the Labels on the app to whatever is in the text fields when the submit button is pushed.
         if let newTitle = textField.text {
             appTitle.text = newTitle
         }
         if let newTitleTwo = textFieldTwo.text {
             appTitle2.text = newTitleTwo
         }
+        //the code on line 30 will give a popup alert
+        let alertController = UIAlertController(title: "Thanks for pushing the button", message: "you da best", preferredStyle: UIAlertController.Style.alert)
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
