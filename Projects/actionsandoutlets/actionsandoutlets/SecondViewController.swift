7bf7f5444e272c52aa4a77705288710eb9898e4c
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-07-22 12:26:41 -0700

    Pushing actions and outlets lesson from July 22

---
+++ Projects/actionsandoutlets/actionsandoutlets/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  actionsandoutlets
+//
+//  Created by Ashanti Williams on 7/22/20.
+//  Copyright © 2020 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
